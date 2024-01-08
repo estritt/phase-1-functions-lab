6a6c6bbccc8e8a8d35d0e541ca7ccbd15f0ebf38
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-01-08 15:26:26 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return (264* distanceFromHqInBlocks(someValue));
+}
+
+function distanceTravelledInFeet(start, stop) {
+    return (264 * Math.abs(stop - start));
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance > 2500) {
+        return 'cannot travel that far';
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance > 400) {
+        return 0.02 * (distance - 400);
+    } else if (distance > 0) {
+        return 0;
+    } else {
+        return 'care to try again?'
+    }
+}
